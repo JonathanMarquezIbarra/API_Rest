1622a66e347cf1f4637adc1b3ba55fa09679723e
JonathanMarquezIbarra <jonathan.marquib@gmail.com>
2022-09-16 21:50:01 -0500

    Bonus upgrade

--- app/src/main/java/com/cursosant/android/stores/mainModule/model/MainInteractor.kt
+++ app/src/main/java/com/cursosant/android/stores/mainModule/model/MainInteractor.kt
@@ -1,9 +1,16 @@
 package com.cursosant.android.stores.mainModule.model
 
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 import com.cursosant.android.stores.StoreApplication
 import com.cursosant.android.stores.common.entities.StoreEntity
+import com.cursosant.android.stores.common.utils.Constants
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
+import java.net.URL
 
 /****
  * Project: Stores
@@ -19,9 +26,42 @@ import org.jetbrains.anko.uiThread
 class MainInteractor {
 
     fun getStores(callback: (MutableList<StoreEntity>) -> Unit){
+        val url = Constants.STORES_URL + Constants.GET_ALL_PATH
+
+        var storeList = mutableListOf<StoreEntity>()
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, { response ->
+
+            //val status = response.getInt(Constants.STATUS_PROPERTY)
+            val status = response.optInt(Constants.STATUS_PROPERTY, Constants.ERROR)
+            Log.i("status", status.toString())
+
+            if (status == Constants.SUCCESS){
+
+                val jsonList = response.getJSONArray(Constants.STORES_PROPERTY)?.toString()
+                if (jsonList != null) {
+                    val mutableListType = object : TypeToken<MutableList<StoreEntity>>() {}.type
+                    storeList = Gson().fromJson(jsonList, mutableListType)
+
+                    callback(storeList)
+                    return@JsonObjectRequest
+                }
+            }
+            callback(storeList)
+
+        }, {
+            it.printStackTrace()
+            callback(storeList)
+        })
+        StoreApplication.storeAPI.addToRequestQueue(jsonObjectRequest)
+    }
+
+    fun getStoresRoom(callback: (MutableList<StoreEntity>) -> Unit){
         doAsync {
             val storeList = StoreApplication.database.storeDao().getAllStores()
             uiThread {
+                val json = Gson().toJson(storeList)
+                Log.i("Gson", json)
                 callback(storeList)
             }
         }
--- app/src/main/java/com/cursosant/android/stores/mainModule/viewModel/MainViewModel.kt
+++ app/src/main/java/com/cursosant/android/stores/mainModule/viewModel/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.cursosant.android.stores.common.entities.StoreEntity
+import com.cursosant.android.stores.common.utils.Constants
 import com.cursosant.android.stores.mainModule.model.MainInteractor
 
 /****
@@ -26,18 +27,26 @@ class MainViewModel: ViewModel() {
         interactor = MainInteractor()
     }
 
-    private val stores: MutableLiveData<List<StoreEntity>> by lazy {
-        MutableLiveData<List<StoreEntity>>().also {
+    private val showProgress: MutableLiveData<Boolean> = MutableLiveData()
+
+    private val stores: MutableLiveData<MutableList<StoreEntity>> by lazy {
+        MutableLiveData<MutableList<StoreEntity>>().also {
             loadStores()
         }
     }
 
-    fun getStores(): LiveData<List<StoreEntity>>{
+    fun getStores(): LiveData<MutableList<StoreEntity>>{
         return stores
     }
 
+    fun isShowProgress(): LiveData<Boolean>{
+        return showProgress
+    }
+
     private fun loadStores(){
+        showProgress.value = Constants.SHOW
         interactor.getStores {
+            showProgress.value = Constants.HIDE
             stores.value = it
             storeList = it
         }
